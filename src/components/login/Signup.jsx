aa1f79481f8fe5e4f1fbca9438e0f086ef9ff842
gayatriponnada <gayatriponnada160@gmail.com>
2025-12-04 20:30:42 +0530

    validation added in signup

--- src/components/login/Signup.jsx
+++ src/components/login/Signup.jsx
@@ -7,9 +7,36 @@ const Signup = () => {
     password: "",
   });
 
-const handleClick = () => {
-  console.log(details);
-}  
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    const newError = {};
+
+    if (!details.fullName) {
+      newError.fullName = "Full Name is required";
+    } else if (details.fullName.length < 3 || details.fullName.length > 50) {
+      newError.fullName = "Full Name must be between 3 and 50 characters";
+    } else if (!/^[a-zA-Z\s]+$/.test(details.fullName)) {
+      newError.fullName = "Full Name can only contain letters and spaces";
+    }
+
+    if (!details.email) {
+      newError.email = "Email is required";
+    } else if (!/^\S+@\S+\.\S+$/.test(details.email)) {
+      newError.email = "Invalid email";
+    }
+    if (!details.password) {
+      newError.password = "Password is required";
+    } else if (details.password.length < 6) {
+      newError.password = "Password must be at least 6 characters long";
+    }
+
+    setErrors(newError);
+  };
+
+  const handleValidate = () => {
+    if (!validate()) return;
+  };
   return (
     <div className="flex items-center justify-center h-[89vh] ">
       <div className="flex flex-col gap-5 w-[28vw]  h-[80vh] rounded-xl border border-(--border-primary) p-4 shadow-xl">
@@ -28,6 +55,11 @@ const handleClick = () => {
             type="text"
             placeholder="Enter your full name"
           />
+          {errors.fullName && (
+            <p className="text-red-400 text-xs font-normal">
+              {errors.fullName}
+            </p>
+          )}
         </div>
         <div>
           <div className="text-sm text-gray-600">Email</div>
@@ -40,6 +72,9 @@ const handleClick = () => {
             type="text"
             placeholder="Enter your Email"
           />
+          {errors.email && (
+            <p className="text-red-400 text-xs font-normal">{errors.email}</p>
+          )}
         </div>
         <div className="flex flex-col gap-1">
           <div className="text-sm text-gray-600 ">Password</div>
@@ -52,10 +87,15 @@ const handleClick = () => {
             type="password"
             placeholder="Enter your Password"
           />
+          {errors.password && (
+            <p className="text-red-400 text-xs font-normal">
+              {errors.password}
+            </p>
+          )}
         </div>
         <div
           className=" flex bg-primary justify-center items-center rounded-xl p-2 cursor-pointer"
-          onClick={handleClick}
+          onClick={handleValidate}
         >
           <button className="text-white cursor-pointer">Create Account</button>
         </div>
