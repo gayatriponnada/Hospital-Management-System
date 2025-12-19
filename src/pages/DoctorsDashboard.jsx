85f99030428b3d06d48ceca0f728f22ab4a056a7
gayatriponnada <gayatriponnada160@gmail.com>
2025-12-19 20:16:22 +0530

    appointments card added

--- src/pages/DoctorsDashboard.jsx
+++ src/pages/DoctorsDashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DoctorCards from "../components/doctors-dashboard/DoctorCards";
+import DoctorAppointments from "../components/doctors-dashboard/DoctorAppointments";
 
 const DoctorsDashboard = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-2 h-[85vh]">
       <DoctorCards />
+      <DoctorAppointments />
     </div>
   );
 };
