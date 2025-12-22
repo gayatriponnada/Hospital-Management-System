8e2c73cfa4088f0da91ace54468481b21311d250
gayatriponnada <gayatriponnada160@gmail.com>
2025-12-22 10:54:01 +0530

    appointments request card added

---
+++ src/components/doctors-dashboard/AppointmentRequestCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppointmentRequestCard = () => {
+	return (
+		<div>AppointmentRequestCard</div>
+	)
+}
+
+export default AppointmentRequestCard
--- src/components/doctors-dashboard/DoctorAppointments.jsx
+++ src/components/doctors-dashboard/DoctorAppointments.jsx
@@ -87,12 +87,6 @@ const DoctorAppointments = () => {
               <div className="text-lg font-semibold text-neutral ">{appointment.time}</div>
             </div>
           ))}
-
-          {/* <div className="avatar avatar-placeholder">
-            <div className="bg-neutral text-neutral-content w-24 rounded-full">
-              <span className="text-3xl">D</span>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
